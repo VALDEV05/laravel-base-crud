b73e61f651d9018f8f9c11222db83d3daa398ccd
Valerio Corda <corda.valerio05@GMAIL.COM>
2022-01-19 17:50:23 +0100

    Fixed bug

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
 
         <!-- Styles -->
-        <link rel="stylesheet" href="{{'css/app.css'}}">
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
         
         <!-- Script -->
         <script src="{{asset('js/app.js')}}" defer></script>

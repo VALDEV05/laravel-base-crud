067b866c11a4258154d0294f05c5ed26dc78717a
Valerio Corda <corda.valerio05@GMAIL.COM>
2022-01-20 09:06:54 +0100

    Add style comics.show

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -2,37 +2,60 @@
 
 
 @section('content')
-    <div id="comics_wrapper">
-        <div class="jumbotron"></div>
-        <!-- /.jumbotron -->
-        
-        <!-- /.sectionComics -->
-        <div id="merchandising" class="d-flex align-items-center">
-            <div class="container row m-auto py-3">
-                <div class="col text-center d-flex align-items-center">
-                    <img src="{{asset('img/buy-comics-digital-comics.png')}}" alt="" />
-                    <p class="ms-2">DIGITAL COMICS</p>
+    <div id="comic">
+        <div id="jumbo_wrap">
+            <div id="jumbotron"></div>
+            <!-- #jumbotron -->
+            <div class="banner"></div>
+            <!-- /.banner -->
+            <div class="container">
+                <div id="poster_wrap">
+                    <div class="poster">
+                        <img src="{{$comic['thumb']}}" alt="">
+                    </div>
+                    <!-- /.poster -->
                 </div>
-                <div class="col text-center d-flex align-items-center">
-                    <img src="{{asset('img/buy-comics-merchandise.png')}}" alt="" />
-                    <p class="ms-2">DC MERCHADISE</p>
-                </div>
-                <div class="col text-center d-flex align-items-center">
-                    <img src="{{asset('img/buy-comics-subscriptions.png')}}" alt="" />
-                    <p class="ms-2">SUBSCTIPTION</p>
-                </div>
-                <div class="col text-center d-flex align-items-center">
-                    <img src="{{asset('img/buy-comics-shop-locator.png')}}" alt="" />
-                    <p class="ms-2">COMIC SHOP LOCATOR</p>
-                </div>
-                <div class="col text-center d-flex align-items-center">
-                    <img src="{{asset('img/buy-dc-power-visa.svg')}}" alt="" />
-                    <p class="ms-2">CD POWER VISA</p>
+                
+            </div>
+            <!-- /.container -->
+        </div>
+        <!-- /#jumbo_wrap -->
+        <div id="comic_info">
+            <div class="container">
+                <div class="row">
+                    <div class="col-8">
+                        <div id="title">
+                            <h1 class="text-uppercase">{{$comic['title']}}</h1>
+                        </div>
+                        <!-- /#title -->
+                        <div id="info" class="my-2">
+                            <div id="price">
+                                <p class="mb-0">
+                                    U.S. Price: <span class="fw-bolder">${{$comic['price']}}</span>
+                                </p>
+                                <p class="mb-0 status text-uppercase fw-bolder">
+                                    available
+                                </p>
+                            </div>
+                            <div id="availability">
+                                Check Availability <i class="fas fa-caret-down"></i>
+                            </div>
+                            <!-- /#availability -->
+                        </div>
+                        <!-- /#price -->
+                        <div id="description">
+                            <p class="mb-0">
+                                {{$comic['description']}}
+                            </p>
+                        </div>
+                        <!-- /#description -->
+                        
+                    </div>   
                 </div>
             </div>
-        
         </div>
-        <!-- /#marchandising -->
+        <!-- /#comic_info -->
     </div>
-    <!-- /#comics_wrapper -->
+    <!-- /#comic -->
 @endsection
+  
